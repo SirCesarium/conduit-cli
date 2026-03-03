132c3f68c59c096cd5701d8cb85087f8be8dc2d6
SirCesarium <cesarium3600@gmail.com>
2026-03-03 19:07:04 -0400

    fix: update get versions function

--- src/modrinth/models.rs
+++ src/modrinth/models.rs
@@ -1,4 +1,5 @@
 use serde::{Deserialize, Serialize};
+use std::collections::HashMap;
 
 #[derive(Debug, Serialize, Deserialize)]
 pub struct SearchResult {
@@ -17,9 +18,19 @@ pub struct ProjectHit {
 
 #[derive(Debug, Serialize, Deserialize)]
 pub struct Version {
+    pub id: String,
+    pub project_id: String,
     pub name: String,
     pub version_number: String,
     pub files: Vec<ModFile>,
+    pub dependencies: Vec<Dependency>,
+}
+
+#[derive(Debug, Serialize, Deserialize)]
+pub struct Dependency {
+    pub project_id: Option<String>,
+    pub version_id: Option<String>,
+    pub dependency_type: String,
 }
 
 #[derive(Debug, Serialize, Deserialize)]
@@ -28,4 +39,16 @@ pub struct ModFile {
     pub filename: String,
     pub size: u64,
     pub primary: bool,
+    pub hashes: HashMap<String, String>,
+}
+
+#[derive(Debug, Serialize, Deserialize)]
+pub struct Project {
+    pub id: String,
+    pub slug: String,
+    pub title: String,
+    pub description: String,
+    pub project_type: String,
+    pub downloads: i32,
+    pub icon_url: Option<String>,
 }
--- src/modrinth/versions.rs
+++ src/modrinth/versions.rs
@@ -3,8 +3,21 @@ use crate::modrinth::models::Version;
 use super::client::ModrinthAPI;
 
 impl ModrinthAPI {
-    pub async fn get_versions(&self, id_or_slug: &str) -> Result<Vec<Version>, reqwest::Error> {
-        let url = format!("{}/project/{}/version", self.base_url, id_or_slug);
+    pub async fn get_versions(
+        &self,
+        id_or_slug: &str,
+        loader: Option<&str>,
+        game_version: Option<&str>,
+    ) -> Result<Vec<Version>, reqwest::Error> {
+        let mut url = format!("{}/project/{}/version?", self.base_url, id_or_slug);
+
+        if let Some(l) = loader {
+            url.push_str(&format!("loaders=[\"{}\"]&", l));
+        }
+        if let Some(gv) = game_version {
+            url.push_str(&format!("game_versions=[\"{}\"]&", gv));
+        }
+
         self.client
             .get(url)
             .send()
